ea0d4d6761cc1112ad58160134cf150d44921cbb
I1 <egeguler2003@gmail.com>
2024-12-13 19:01:13 +0300

    transactions

--- src/app/services/listing.service.ts
+++ src/app/services/listing.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import { Firestore, collection, getDocs, query, where,doc, setDoc, deleteDoc, collectionGroup, getDoc } from '@angular/fire/firestore';
 import { Auth } from '@angular/fire/auth';
 import { Car } from './car.service'; // Import the Car interface from CarService
-import { Observable, from, of } from 'rxjs';
+import { Observable, from, of, switchMap } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { BookingService } from './booking.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ListingService {
-  constructor(private firestore: Firestore, private auth: Auth) {}
+  constructor(private firestore: Firestore, private auth: Auth, private bookingService: BookingService) {}
 
   // Add a new listing to Firestore under the authenticated user
   async addListing(listing: Car): Promise<void> {
@@ -47,19 +48,25 @@ export class ListingService {
   }
   getAllListings(): Observable<Car[]> {
     const listingsQuery = collectionGroup(this.firestore, 'listings');
-
-    // Fetch and map Firestore documents to Car objects
-    return from(
-      getDocs(listingsQuery).then((snapshot) =>
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          ...doc.data(),
-        } as Car))
-      )
-    ).pipe(
-      catchError(() => of([])) 
+  
+    return this.bookingService.getAllBookedCarIds().pipe(
+      // Use the booking IDs to filter out listings
+      switchMap((bookedCarIds) =>
+        from(
+          getDocs(listingsQuery).then((snapshot) =>
+            snapshot.docs
+              .map((doc) => ({
+                id: doc.id,
+                ...doc.data(),
+              } as Car))
+              .filter((listing) => !bookedCarIds.includes(listing.id)) // Exclude booked listings
+          )
+        )
+      ),
+      catchError(() => of([])) // Return an empty array in case of an error
     );
   }
+  
 
   async removeListing(listingId: string): Promise<void> {
     const user = this.auth.currentUser;
